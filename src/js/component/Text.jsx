193cbc62482b21fa99f04da0d68bfe6ad0f3d7fc
MarOlivieri <141793518+MarOlivieri@users.noreply.github.com>
2024-05-20 16:01:54 +0000

    borrador

---
+++ src/js/component/Text.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+
+const Text = () => {
+    return (
+      <> 
+      <h1>"A Warm Welcome"</h1>
+      <p>Lorem ipsumm.............lorem ipsum</p>
+      <button>Call to action!</button>
+      </>
+    );
+  };
+  export default Text;
+  
